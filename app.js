c353a502de089344c3a9e208ba7e674ceea044aa
Rameez <rameez.arif1@gmail.com>
2016-09-25 23:30:17 -0400

    configured ajax call and rendered data to the screen

--- app.js
+++ app.js
@@ -14,18 +14,41 @@ function getStockInfo(stockSymbol) {
 		data: {symbol: stockSymbol},
 		url: "http://dev.markitondemand.com/Api/v2/Quote/jsonp",
 		dataType: "jsonp"
+	
 	}).done(function(data) {
-		
-		var myStock = new Stock(data.Name, data.LastPrice, data.High, data.Low, data.Change, data.ChangePercent);
-		console.log(myStock.name);
+		if (data.hasOwnProperty('message'))
+			alert("enter a vailid stock name");
+		else {
+			var myStock = new Stock(data.Name, data.LastPrice, data.High, data.Low, data.Change, data.ChangePercent);
+			renderInfo(myStock);
+		}
+
+	}).fail(function(){
+		alert("error");
 	});
 }
 
+
+function renderInfo(stock) {
+	$('#name').text(stock.name);
+	$('#current-price').text(stock.currentPrice);
+	$('#change').text(stock.change.toPrecision(3));
+	$('#high-price').text(stock.high);
+	$('#low-price').text(stock.low);
+	console.log(stock);
+}
+
+
 $('#submit-btn').click(function() {
-	var stockSymbol = $('#stock-name').text();
-	console.log(stockSymbol);
+	var stockSymbol = $('#stock-name').val();
 	getStockInfo(stockSymbol);
+
 });
+
+//TODO: style html
+//		spacing on stock info
+//		create graph
+// 		error handling
 
 
 
